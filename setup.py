433b249133662977d6676bb0e1132762f6bea4ff
AndreaBenassi1 <andrea.benassi@ict-group.it>
2024-10-29 18:28:01 +0100

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup, find_packages
 
+
+# Funzione per leggere il file requirements.txt
+def read_requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+    
 setup(
     name='video-analysis',
     version='0.1',
     packages=find_packages(),
+    install_requires=read_requirements(),  # Ottieni dipendenze dinamicamente
+    entry_points={
+        'console_scripts': [
+            'run-video-analysis=app.main:main',
+        ],
+    },
 )
